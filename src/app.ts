9e70065c405f668121970d9186394962d1d8fcfd
coderboy-raiyan <tajkierhaque@gmail.com>
2026-01-20 20:54:25 +0600

    env example and cors issue fixed

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,18 @@ import { toNodeHandler } from 'better-auth/node';
 import cookieParse from 'cookie-parser';
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import config from './config';
 import { auth } from './lib/auth';
 import globalErrorHandler from './middlewares/globalErrorHandler.middleware';
 import notFound from './middlewares/notFound.middleware';
 import router from './router';
 const app: Application = express();
 
+app.use(cors({ origin: [config.APP_URL], credentials: true }));
 app.all('/api/auth/*splat', toNodeHandler(auth));
 
 app.use(express.json());
 app.use(cookieParse());
-app.use(cors({ origin: '*', credentials: true }));
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({
